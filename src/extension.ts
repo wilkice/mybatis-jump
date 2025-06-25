c965573999f1389a0a7191b8a85e5342d49e14ac
darcy <yanjiabinchn@gmail.com>
2025-06-25 22:02:47 +0800

    feat: remove unused quick start command hello world

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,7 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mybatis-jump-mapper-xml" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('mybatis-jump-mapper-xml.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World');
-	});
-
-	context.subscriptions.push(disposable);
-
-	const goToMybatisDefinition = vscode.commands.registerCommand('mybatis-jump-mapper-xml.goToMybatisDefinition', async () => {
+	const goMapperDefinition = vscode.commands.registerCommand('mybatis-jump-mapper-xml.goMapperDefinition', async () => {
 		const editor = vscode.window.activeTextEditor;
 		if (!editor) {
 			return;
@@ -89,7 +74,7 @@ export function activate(context: vscode.ExtensionContext) {
 		xmlEditor.revealRange(range, vscode.TextEditorRevealType.InCenter);
 	});
 	
-	context.subscriptions.push(goToMybatisDefinition);
+	context.subscriptions.push(goMapperDefinition);
 }
 
 // This method is called when your extension is deactivated
